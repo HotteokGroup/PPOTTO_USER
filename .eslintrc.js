9db8397e42f03c309fda5de5ea9ebe6cbccda356
Seongrok.lee <55491354+argon1025@users.noreply.github.com>
2023-09-01 00:55:05 +0000

    [#1] 프로젝트 환경, Eslint, Prettier 구성 (#3)
    * chore: 노드 버전명시
    
    * chore: TypeScript 버전명시
    
    * feat: airbnb 룰, import/order 추가
    
    * chore: 에디터 설정 변경
    
    * feat: 환경설정파일 구성

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,9 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:import/recommended',
   ],
   root: true,
   env: {
@@ -21,5 +22,37 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "import/prefer-default-export": "off",
+    "class-methods-use-this": "off",
+    // 가져오기 누락시 오류
+    "import/no-unresolved": "error",
+    // import 순서정의
+    "import/order":[
+      "error",
+      {
+        "groups": ["builtin", "internal", "external", "type", "unknown"],
+        "pathGroups": [
+          {
+            "pattern": "@nestjs/*",
+            "group": "builtin",
+            "position": "before"
+          },
+          {
+            "pattern": "*.controller",
+            "group": "internal",
+            "position": "before"
+          },
+          {
+            "pattern": "*.service",
+            "group": "internal",
+            "position": "after"
+          }
+        ],
+        "alphabetize": {
+          "order": "asc"
+        },
+        "newlines-between": "always",
+      }
+    ]
   },
 };
