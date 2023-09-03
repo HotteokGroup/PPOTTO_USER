1c95d79b6df05a3059a84e4342797ac1dc81bc1f
argon1025 <argon1025@gmail.com>
2023-09-04 01:47:36 +0900

    feat: 유저 가입처리 command

---
+++ src/user/command/completed-sign-up/completed-sign-up-user.command.ts
@@ -0,0 +1,14 @@
+export class CompletedSignUpUserCommand {
+  userId: number;
+
+  constructor(params: CompletedSignUpUserCommand) {
+    Object.assign(this, params);
+  }
+}
+export class CompletedSignUpUserCommandResult {
+  userId: number;
+
+  constructor(params: CompletedSignUpUserCommand) {
+    Object.assign(this, params);
+  }
+}
