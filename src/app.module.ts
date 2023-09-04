48be107920d322528a91631de430b6862c6e5b60
argon1025 <argon1025@gmail.com>
2023-09-04 06:27:03 +0000

    refactor: ValidationPipe 불필요 옵션 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,6 @@ import { UserModule } from './user/user.module';
       provide: APP_PIPE,
       useValue: new ValidationPipe({
         transform: true, // transform payload to DTO
-        whitelist: true, // remove unknown properties from payload
         forbidUnknownValues: true, // throw error if unknown properties are present
         exceptionFactory: (errors: ValidationError[]) => {
           if (!errors[0]?.constraints) return new BadRequestException(ERROR_CODE.INVALID_DATA);
