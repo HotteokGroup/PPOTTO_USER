193a5902f759a40977f57c091682d4bda8d92601
argon1025 <argon1025@gmail.com>
2023-09-14 01:13:08 +0900

    feat: 기타 환경설정

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,29 @@ import { writeFileSync } from 'fs';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as Sentry from '@sentry/node';
 
 import { AppModule } from './app.module';
+import { CustomLogger } from './lib/logger/custom-logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
   const configService = app.get<ConfigService>(ConfigService);
 
   const servicePort = configService.getOrThrow<string>('SERVICE_PORT');
   const environment = configService.getOrThrow<string>('ENVIRONMENT');
+
+  app.useLogger(new CustomLogger());
+
+  if (environment !== 'local') {
+    Sentry.init({
+      dsn: 'https://72c8804c0e385bc74566c73211d03f73@o4505860408147968.ingest.sentry.io/4505873814061056',
+      environment,
+    });
+    app.use(Sentry.Handlers.requestHandler());
+  }
 
   if (environment !== 'prod') {
     const config = new DocumentBuilder()
