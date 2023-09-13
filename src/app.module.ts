397f4ef3d9aa7aa5fc07e464f2f58cd7af9524f6
argon1025 <argon1025@gmail.com>
2023-09-14 01:12:47 +0900

    feat: all-exception 필터 정의

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,12 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FileStoreModule } from './file-store/file-store.module';
+import { AllExceptionsFilter } from './lib/exception/all-exception.filter';
 import { ERROR_CODE } from './lib/exception/error.constant';
 import { TermsOfServiceModule } from './terms-of-service/terms-of-service.module';
 import { UserModule } from './user/user.module';
@@ -40,6 +41,7 @@ import { VerificationModule } from './verification/verification.module';
       }),
     },
     { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
+    { provide: APP_FILTER, useClass: AllExceptionsFilter },
   ],
 })
 export class AppModule {}
