25d23a9871c3f286bac3111b66441f66b712b6d7
argon1025 <argon1025@gmail.com>
2023-11-17 08:18:02 +0000

    feat: 워크플로우 환경 구성

--- src/lib/prisma/prisma.service.ts
+++ src/lib/prisma/prisma.service.ts
@@ -1,9 +1,17 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly NODE_ENV: string = this.configService.getOrThrow('NODE_ENV');
+  constructor(private readonly configService: ConfigService) {
+    super();
+  }
   async onModuleInit() {
+    if (this.NODE_ENV === 'offline') {
+      return;
+    }
     await this.$connect();
   }
 }
